956444d566301a5631f291678685f874bee29e7e
TechTrooper448482 <mowaiskhan658@gmail.com>
2025-07-29 00:01:46 +0500

    Login and Signup Screen UI completed

--- new_app/lib/config/routes/app_routes.dart
+++ new_app/lib/config/routes/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:clothing_store_app/features/auth/presentation/screens/signup_screen.dart';
 import 'package:clothing_store_app/features/splash/presentation/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,7 @@ class AppRoutes{
   static const String splashScreen = '/';
   static const String loginScreen = '/loginScreen';
   static const String homeScreen = '/homeScreen';
+  static const String signupScreen = '/signupScreen';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -15,6 +17,8 @@ class AppRoutes{
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case loginScreen:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case signupScreen:
+        return MaterialPageRoute(builder: (_) => const SignupScreen());
       default:
         return MaterialPageRoute(
             builder: (_) => const Scaffold(
--- new_app/lib/features/auth/presentation/screens/signup_screen.dart
+++ new_app/lib/features/auth/presentation/screens/signup_screen.dart
@@ -1,20 +1,16 @@
+import 'package:clothing_store_app/config/routes/app_routes.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../config/routes/app_routes.dart';
-
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-
-
-
-
-
+class _SignupScreenState extends State<SignupScreen> {
+  bool checkboxValue = true;
 
   @override
   Widget build(BuildContext context) {
@@ -27,27 +23,54 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Column(
                 children: [
                   SizedBox(
-                    height: 120,
+                    height: 80,
                   ),
                   Text(
-                    "Sign In",
+                    "Create Account",
                     style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w500,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                   SizedBox(
                     height: 12,
                   ),
                   Text(
-                      "Hi Welcome back, you've been missed",
+                    "Fill your information below or register\n with your social account.",
                     style: TextStyle(
-                        fontSize: 12,
+                      fontSize: 12,
                       color: Colors.black54,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 300.0),
+                    child: Text(
+                      "Name",
+                      style: TextStyle(fontWeight: FontWeight.normal),
                     ),
                   ),
                   SizedBox(
-                    height: 80,
+                    height: 4,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      isDense: true,
+                      contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                      hintText: "John Doe",
+                      hintStyle: TextStyle(
+                        color: Colors.black54,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(120.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
                   ),
                   Padding(
                     padding: const EdgeInsets.only(right: 300.0),
@@ -99,22 +122,40 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 200.0),
-                    child: TextButton(
-                        onPressed: (){},
-                        child: Text(
-                            "Forgot Password?",
-                          style: TextStyle(
-                            color: Colors.brown,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.brown,
-                          ),
-                        ),
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CheckboxListTile(
+                          value: checkboxValue,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              checkboxValue = value!;
+                            });
+                          },
+                        activeColor: Colors.brown,
+                        title: Text.rich(TextSpan(
+                          children: [
+                            TextSpan(text: "Agree with"),
+                            TextSpan(
+                              text: "Terms & Condition",
+                              style: TextStyle(
+                                color: Colors.brown,
+                                decoration: TextDecoration.underline,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                // On Tap Code Here
+                                },
+                            ),
+                          ],
+                        )),
+                        controlAffinity: ListTileControlAffinity.leading,
+                        contentPadding: EdgeInsets.zero,
+                      ),
+                    ],
                   ),
                   SizedBox(
-                    height: 40,
+                    height: 20,
                   ),
                   Container(
                     // height: 40,
@@ -128,11 +169,12 @@ class _LoginScreenState extends State<LoginScreen> {
                       color: Colors.brown,
                     ),
                     child: Center(
-                        child: Text(
-                          "Sign In",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),),
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                   SizedBox(
@@ -149,7 +191,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           thickness: 1,
                         ),
                       ),
-                      Text("Or sign in with"),
+                      Text("Or sign up with"),
                       Expanded(
                         child: Divider(
                           indent: 12,
@@ -184,7 +226,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         child: Image(
                           image: NetworkImage(
                               'https://pixsector.com/cache/56f2646e/avd5cee2ff5ea9da4d328.png'
-                              // 'https://cdn-icons-png.flaticon.com/512/0/747.png'
+                            // 'https://cdn-icons-png.flaticon.com/512/0/747.png'
                           ),
                         ),
                       ),
@@ -201,9 +243,9 @@ class _LoginScreenState extends State<LoginScreen> {
                         child: Image(
                           // height: 20,
                           //   width: 20,
-                            image: NetworkImage(
-                                'https://img.icons8.com/color/512/google-logo.png'
-                            ),
+                          image: NetworkImage(
+                              'https://img.icons8.com/color/512/google-logo.png'
+                          ),
                         ),
                       ),
                       Container(
@@ -232,19 +274,19 @@ class _LoginScreenState extends State<LoginScreen> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Don't have an account?"),
+                      Text("Already have an account?"),
                       TextButton(
-                          onPressed: (){
-                            Navigator.pushReplacementNamed(context, AppRoutes.signupScreen);
-                          },
-                          child: Text(
-                              "Sign Up",
-                            style: TextStyle(
-                              color: Colors.brown,
-                              decoration: TextDecoration.underline,
-                              decorationColor: Colors.brown,
-                            ),
+                        onPressed: (){
+                          Navigator.pushReplacementNamed(context, AppRoutes.loginScreen);
+                        },
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                            color: Colors.brown,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.brown,
                           ),
+                        ),
                       ),
                     ],
                   ),
